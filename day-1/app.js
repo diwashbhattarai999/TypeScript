555a37f0b494fe83500374da305e0548b88db6f1
Diwash <diwashb999@gmail.com>
2023-04-20 22:08:39 +0545

    Core Types: object

--- day-1/app.js
+++ day-1/app.js
@@ -1,19 +1,11 @@
-function add(n1, n2, showResult, phrase) {
-    // console.log(typeof number1);
-    // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //     throw new Error("Incorrect input!")
-    //  }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5; //5.0 is same as 5 in js
-var number2 = 2.8;
-var printRestult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printRestult, resultPhrase);
-// console.log(result);
+var person = {
+    name: "Diwash",
+    age: 21,
+};
+console.log(person.name);
+//When we hover over person, we get:
+// const person: {
+//     name: string;
+//     age: number;
+// }
+//This is similar to object but it is not an object. This is the object type inferred by TypeScript
