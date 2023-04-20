a7d086d26538fb4d27430fa9487d903f494b08f5
Diwash <diwashb999@gmail.com>
2023-04-20 22:23:53 +0545

    Core Types: Array

--- day-1/app.js
+++ day-1/app.js
@@ -1,7 +1,24 @@
+//Generic type assign
+// const person: object = {
+//     name: "Diwash",
+//     age: 21,
+// }
+//Specific type assign
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: "Diwash",
+//     age: 21,
+// };
+//Specific type assign(automatic)
 var person = {
     name: "Diwash",
     age: 21,
+    hobbies: ["Coding", "Sports"],
 };
+var favoriteActivities;
+favoriteActivities = ["Sports"];
 console.log(person.name);
 //When we hover over person, we get:
 // const person: {
@@ -9,3 +26,7 @@ console.log(person.name);
 //     age: number;
 // }
 //This is similar to object but it is not an object. This is the object type inferred by TypeScript
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- day-1/app.ts
+++ day-1/app.ts
@@ -1,10 +1,10 @@
-//Generic type assign
+//*Generic type assign
 // const person: object = {
 //     name: "Diwash",
 //     age: 21,
 // }
 
-//Specific type assign
+//*Specific type assign
 // const person: {
 //     name: string;
 //     age: number;
@@ -13,17 +13,27 @@
 //     age: 21,
 // };
 
-//Specific type assign(automatic)
+//*Specific type assign(automatic)
+
 const person = {
-    name: "Diwash",
-    age: 21,
-}
- 
+  name: "Diwash",
+  age: 21,
+  hobbies: ["Coding", "Sports"],
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
 
-//When we hover over person, we get:
+//*When we hover over person, we get:
 // const person: {
 //     name: string;
 //     age: number;
 // }
-//This is similar to object but it is not an object. This is the object type inferred by TypeScript
+//?This is similar to object but it is not an object. This is the object type inferred by TypeScript
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //   console.log(hobby.map) //!Thorws an error since hobby is string not an array.
+}
