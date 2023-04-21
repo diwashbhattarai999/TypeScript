58e7dc2150c9d87de915f52acffc38c913fdba15
Diwash <diwashb999@gmail.com>
2023-04-21 07:42:45 +0545

    Custom Types:Tuples, enum & any

--- day-1/obj-arr-tup-enum-any.ts
+++ day-1/obj-arr-tup-enum-any.ts
@@ -3,6 +3,7 @@
 //     name: "Diwash",
 //     age: 21,
 // }
+
 //*Specific type assign
 // const person: {
 //     name: string;
@@ -11,7 +12,9 @@
 //     name: "Diwash",
 //     age: 21,
 // };
+
 //*Specific type assign(automatic)
+
 // const person: {
 //     name: string;
 //     age: number;
@@ -23,46 +26,56 @@
 //   hobbies: ["Coding", "Sports"],
 //   role: [2, "author"],
 // };
+
 // const ADMIN = 0;
 // const READ_ONLY = 1;
 // const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "Diwash",
-    age: 21,
-    hobbies: ["Coding", "Sports"],
-    role: Role.ADMIN,
+
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 100,
+  AUTHOR = 200,
+}
+
+const person = {
+  name: "Diwash",
+  age: 21,
+  hobbies: ["Coding", "Sports"],
+  role: Role.ADMIN,
 };
+
 if (person.role === Role.ADMIN) {
-    console.log("Is read only");
+  console.log("Is read only");
+} else if (person.role === Role.AUTHOR) {
+    console.log("Is author")
 }
-else if (person.role === Role.AUTHOR) {
-    console.log("Is author");
-}
+
 // const person = {
 //   name: "Diwash",
 //   age: 21,
 //   hobbies: ["Coding", "Sports"],
 //   role: [2, "author"],
 // };
+
 // person.role.push("admin");
 // person.role[1] = 10;
+
 // let favoriteActivities: string[];
 // favoriteActivities = ["Sports"];
+
+let favoriteActivities: any[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
 //*When we hover over person, we get:
 // const person: {
 //     name: string;
 //     age: number;
 // }
 //?This is similar to object but it is not an object. This is the object type inferred by TypeScript
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    //   console.log(hobby.map) //!Thorws an error since hobby is string not an array.
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //   console.log(hobby.map) //!Thorws an error since hobby is string not an array.
 }
