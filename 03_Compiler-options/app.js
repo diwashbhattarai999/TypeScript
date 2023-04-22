8f289e868dc0186879ac27eb1acbbeba44758892
Diwash <diwashb999@gmail.com>
2023-04-22 12:35:55 +0545

    watch mode using tsc fileName.ts -w

---
+++ 03_Compiler-options/app.js
@@ -0,0 +1,8 @@
+var userName = "Diwash Bhattarai";
+console.log("This was easy...");
+console.log(userName);
+//We have to manually compile the .ts file each time using: 
+//tsc filName.ts
+//So, for making development easy, we can use watch mode using:
+//tsc fileName.ts --watch or tsc fileName.ts -w
+//With this watch mode, we don't have to compile each time 
