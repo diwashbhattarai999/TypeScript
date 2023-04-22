91e0cbc82665a7f9d7ee7a2a77d97856846f2dc9
Diwash <diwashb999@gmail.com>
2023-04-22 12:19:59 +0545

    Basic Types in TS

---
+++ Basic-types/app.ts
@@ -0,0 +1,17 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+//unknown is better than using any but still avoid using unknown as much as possible.
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  //   while (true) {}
+}
+
+const result = generateError("An error occurred!", 500);
+console.log(result);
