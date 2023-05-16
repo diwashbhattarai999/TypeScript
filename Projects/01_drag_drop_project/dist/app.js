2b0e3e3bc2145266b35da158b291cdf0740f5692
Diwash <diwashb999@gmail.com>
2023-05-16 20:15:52 +0545

    Filtering projects with enums & fixed duplicate project bug

--- Projects/01_drag_drop_project/dist/app.js
+++ Projects/01_drag_drop_project/dist/app.js
@@ -96,7 +96,13 @@ class ProjectList {
         this.element = importedNode.firstElementChild;
         this.element.id = `${this.type}-projects`;
         projectState.addListener((projects) => {
-            this.assignedProjects = projects;
+            const releventProjects = projects.filter((prj) => {
+                if (this.type === "active") {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
+            });
+            this.assignedProjects = releventProjects;
             this.renderProject();
         });
         this.attach();
@@ -104,6 +110,7 @@ class ProjectList {
     }
     renderProject() {
         const listEl = document.getElementById(`${this.type}-projects-list`);
+        listEl.innerHTML = "";
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement("li");
             listItem.textContent = prjItem.title;
