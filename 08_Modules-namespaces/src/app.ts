e3703728948b06a6b5ea57f74cdf38a8489d272e
Diwash <diwashb999@gmail.com>
2023-05-20 20:35:34 +0545

    Modules in TS

---
+++ 08_Modules-namespaces/src/app.ts
@@ -0,0 +1,9 @@
+// Splitting Code into Multiple Files
+
+/*  ###### Namespaces & File Bundling ###### */
+// 1.Use "namespace" code syntax to group code
+// 2.Per-file or bundled compilation is possible (less imports to manage)
+
+/*  ###### ES6 Imports/Exports ###### */
+// 1. Use ES6 import/export syntax
+// 2. Per-file compilation but single <script> import
